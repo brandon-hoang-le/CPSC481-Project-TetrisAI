d4467e04966022f4b306ed1b2d778a38e5b526ad
Darryn-Wong <Darryn-Wong@users.noreply.github.com>
2021-11-07 20:03:26 -0800

    set up some AI functions

--- tetris.py
+++ tetris.py
@@ -969,7 +969,10 @@ class MovingPiece:
                 # gameClock.fall.preFrame = gameClock.frameTick #Commented out because each seperate down key press and release creates a delay which makes the game easier
 
         # else: # 'collided'
-
+    def display(self):
+        for i in range(4):
+            print("(", self.blocks[i].currentPos.row,
+                  self.blocks[i].currentPos.col, ")")
 # Class for the blocks of the moving piece. Each piece is made of 4 blocks in Tetris game
 
 
@@ -1021,122 +1024,122 @@ def gameLoop():
 
     gameExit = False
     while not gameExit:  # Stay in this loop unless the game is quit
-        if mainBoard.piece == "uncreated":
-            for event in list(pygame.event.get()):
+        # if mainBoard.piece == "uncreated":
+        #     for event in list(pygame.event.get()):
 
-                # Looks for quitting event in every iteration (Meaning closing the game window)
+        #         # Looks for quitting event in every iteration (Meaning closing the game window)
 
-                if event.type == pygame.QUIT:
-                    gameExit = True
+        #         if event.type == pygame.QUIT:
+        #             gameExit = True
 
-                if event.type == pygame.KEYDOWN:  # Keyboard keys press events
-                    if event.key == pygame.K_LEFT:
-                        xChange += -1
-                    if event.key == pygame.K_RIGHT:
-                        xChange += 1
-                    if event.key == pygame.K_DOWN:
-                        key.down.status = 'pressed'
-                    if event.key == pygame.K_UP:
-                        if key.rotate.status == 'idle':
-                            key.rotate.trig = True
-                            key.rotate.status = 'pressed'
-                    if event.key == pygame.K_z:
-                        if key.cRotate.status == 'idle':
-                            key.cRotate.trig = True
-                            key.cRotate.status = 'pressed'
-                    if event.key == pygame.K_p:
-                        if key.pause.status == 'idle':
-                            key.pause.trig = True
-                            key.pause.status = 'pressed'
-                    if event.key == pygame.K_r:
-                        if key.restart.status == 'idle':
-                            key.restart.trig = True
-                            key.restart.status = 'pressed'
-                    if event.key == pygame.K_RETURN:
-                        key.enter.status = 'pressed'
+        #         if event.type == pygame.KEYDOWN:  # Keyboard keys press events
+        #             if event.key == pygame.K_LEFT:
+        #                 xChange += -1
+        #             if event.key == pygame.K_RIGHT:
+        #                 xChange += 1
+        #             # if event.key == pygame.K_DOWN:
+        #             #     key.down.status = 'pressed'
+        #             if event.key == pygame.K_UP:
+        #                 # if key.rotate.status == 'idle':
+        #                 key.rotate.trig = True
+        #                 # key.rotate.status = 'pressed'
+        #             if event.key == pygame.K_z:
+        #                 if key.cRotate.status == 'idle':
+        #                     key.cRotate.trig = True
+        #                     key.cRotate.status = 'pressed'
+        #             if event.key == pygame.K_p:
+        #                 if key.pause.status == 'idle':
+        #                     key.pause.trig = True
+        #                     key.pause.status = 'pressed'
+        #             if event.key == pygame.K_r:
+        #                 if key.restart.status == 'idle':
+        #                     key.restart.trig = True
+        #                     key.restart.status = 'pressed'
+        #             if event.key == pygame.K_RETURN:
+        #                 key.enter.status = 'pressed'
 
-                if event.type == pygame.KEYUP:  # Keyboard keys release events
-                    if event.key == pygame.K_LEFT:
-                        xChange += 1
-                    if event.key == pygame.K_RIGHT:
-                        xChange += -1
-                    if event.key == pygame.K_DOWN:
-                        key.down.status = 'released'
-                    if event.key == pygame.K_UP:
-                        key.rotate.status = 'idle'
-                    if event.key == pygame.K_z:
-                        key.cRotate.status = 'idle'
-                    if event.key == pygame.K_p:
-                        key.pause.status = 'idle'
-                    if event.key == pygame.K_r:
-                        key.restart.status = 'idle'
-                    if event.key == pygame.K_RETURN:
-                        key.enter.status = 'idle'
+        #         if event.type == pygame.KEYUP:  # Keyboard keys release events
+        #             if event.key == pygame.K_LEFT:
+        #                 xChange += 1
+        #             if event.key == pygame.K_RIGHT:
+        #                 xChange += -1
+        #             # if event.key == pygame.K_DOWN:
+        #             #     key.down.status = 'released'
+        #             # if event.key == pygame.K_UP:
+        #             #     key.rotate.status = 'idle'
+        #             if event.key == pygame.K_z:
+        #                 key.cRotate.status = 'idle'
+        #             if event.key == pygame.K_p:
+        #                 key.pause.status = 'idle'
+        #             if event.key == pygame.K_r:
+        #                 key.restart.status = 'idle'
+        #             if event.key == pygame.K_RETURN:
+        #                 key.enter.status = 'idle'
 
-                if xChange > 0:
-                    key.xNav.status = 'right'
-                elif xChange < 0:
-                    key.xNav.status = 'left'
-                else:
-                    key.xNav.status = 'idle'
-        else:
-            for event in list(pygame.event.get()) + run_ai(mainBoard, mainBoard.piece, boardColNum, boardRowNum):
+        #         if xChange > 0:
+        #             key.xNav.status = 'right'
+        #         elif xChange < 0:
+        #             key.xNav.status = 'left'
+        #         else:
+        #             key.xNav.status = 'idle'
+        # else:
+        for event in list(pygame.event.get()) + run_ai(mainBoard, boardColNum, boardRowNum):
 
-                # Looks for quitting event in every iteration (Meaning closing the game window)
+            # Looks for quitting event in every iteration (Meaning closing the game window)
 
-                if event.type == pygame.QUIT:
-                    gameExit = True
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-                if event.type == pygame.KEYDOWN:  # Keyboard keys press events
-                    if event.key == pygame.K_LEFT:
-                        xChange += -1
-                    if event.key == pygame.K_RIGHT:
-                        xChange += 1
-                    if event.key == pygame.K_DOWN:
-                        key.down.status = 'pressed'
-                    if event.key == pygame.K_UP:
-                        if key.rotate.status == 'idle':
-                            key.rotate.trig = True
-                            key.rotate.status = 'pressed'
-                    if event.key == pygame.K_z:
-                        if key.cRotate.status == 'idle':
-                            key.cRotate.trig = True
-                            key.cRotate.status = 'pressed'
-                    if event.key == pygame.K_p:
-                        if key.pause.status == 'idle':
-                            key.pause.trig = True
-                            key.pause.status = 'pressed'
-                    if event.key == pygame.K_r:
-                        if key.restart.status == 'idle':
-                            key.restart.trig = True
-                            key.restart.status = 'pressed'
-                    if event.key == pygame.K_RETURN:
-                        key.enter.status = 'pressed'
+            if event.type == pygame.KEYDOWN:  # Keyboard keys press events
+                if event.key == pygame.K_LEFT:
+                    xChange += -1
+                if event.key == pygame.K_RIGHT:
+                    xChange += 1
+                if event.key == pygame.K_DOWN:
+                    key.down.status = 'pressed'
+                if event.key == pygame.K_UP:
+                    if key.rotate.status == 'idle':
+                        key.rotate.trig = True
+                        key.rotate.status = 'pressed'
+                if event.key == pygame.K_z:
+                    if key.cRotate.status == 'idle':
+                        key.cRotate.trig = True
+                        key.cRotate.status = 'pressed'
+                if event.key == pygame.K_p:
+                    if key.pause.status == 'idle':
+                        key.pause.trig = True
+                        key.pause.status = 'pressed'
+                if event.key == pygame.K_r:
+                    if key.restart.status == 'idle':
+                        key.restart.trig = True
+                        key.restart.status = 'pressed'
+                if event.key == pygame.K_RETURN:
+                    key.enter.status = 'pressed'
 
-                if event.type == pygame.KEYUP:  # Keyboard keys release events
-                    if event.key == pygame.K_LEFT:
-                        xChange += 1
-                    if event.key == pygame.K_RIGHT:
-                        xChange += -1
-                    if event.key == pygame.K_DOWN:
-                        key.down.status = 'released'
-                    if event.key == pygame.K_UP:
-                        key.rotate.status = 'idle'
-                    if event.key == pygame.K_z:
-                        key.cRotate.status = 'idle'
-                    if event.key == pygame.K_p:
-                        key.pause.status = 'idle'
-                    if event.key == pygame.K_r:
-                        key.restart.status = 'idle'
-                    if event.key == pygame.K_RETURN:
-                        key.enter.status = 'idle'
+            # if event.type == pygame.KEYUP:  # Keyboard keys release events
+            #     if event.key == pygame.K_LEFT:
+            #         xChange += 1
+            #     if event.key == pygame.K_RIGHT:
+            #         xChange += -1
+            #     if event.key == pygame.K_DOWN:
+            #         key.down.status = 'released'
+            #     if event.key == pygame.K_UP:
+            #         key.rotate.status = 'idle'
+            #     if event.key == pygame.K_z:
+            #         key.cRotate.status = 'idle'
+            #     if event.key == pygame.K_p:
+            #         key.pause.status = 'idle'
+            #     if event.key == pygame.K_r:
+            #         key.restart.status = 'idle'
+            #     if event.key == pygame.K_RETURN:
+            #         key.enter.status = 'idle'
 
-                if xChange > 0:
-                    key.xNav.status = 'right'
-                elif xChange < 0:
-                    key.xNav.status = 'left'
-                else:
-                    key.xNav.status = 'idle'
+            if xChange > 0:
+                key.xNav.status = 'right'
+            elif xChange < 0:
+                key.xNav.status = 'left'
+            else:
+                key.xNav.status = 'idle'
 
         # Whole screen is painted black in every iteration before any other drawings occur
 
--- tetrisAI.py
+++ tetrisAI.py
@@ -14,53 +14,70 @@ class Event():
 counter = 0
 
 
-# def run_ai(game_field, game_figure, game_width, game_height):
-#     global counter
-#     counter += 1
-#     if counter < 3:
-#         return []
-#     counter = 0
-#     e1 = Event(pygame.KEYDOWN, pygame.K_UP)
-#     # e2 = Event(pygame.KEYUP, pygame.K_UP)
-#     return [e1]
-
-def run_ai(game_field, game_figure, game_width, game_height):
+def run_ai(game_field, game_width, game_height):
     global counter
     counter += 1
     if counter < 3:
         return []
     counter = 0
-    rotation_counter = 0
+    rotation_counter = 1
+    e = []
+    # print(simulate(game_field, game_field.piece, game_width, game_height))
     rotation, position = best_rotation_position(
-        game_field, game_figure, game_width, game_height)
+        game_field, game_width, game_height)
+    # print(rotation_counter, rotation, position)
+    # if rotation_counter != rotation:
+    #     e.append(Event(pygame.KEYDOWN, pygame.K_UP))
+    #     e.append(Event(pygame.KEYUP, pygame.K_UP))
+    #     rotation_counter += 1
     if rotation_counter != rotation:
-        e = Event(pygame.KEYDOWN, pygame.K_UP)
+        e.append(Event(pygame.KEYDOWN, pygame.K_UP))
+        e.append(Event(pygame.KEYUP, pygame.K_UP))
         rotation_counter += 1
-    elif game_figure.colNum < position:
-        e = Event(pygame.KEYDOWN, pygame.K_RIGHT)
-    elif game_figure.colNum > position:
-        e = Event(pygame.KEYDOWN, pygame.K_LEFT)
+    elif game_field.piece.blocks[2].currentPos.col < position:
+        e.append(Event(pygame.KEYDOWN, pygame.K_RIGHT))
+        e.append(Event(pygame.KEYUP, pygame.K_RIGHT))
+    elif game_field.piece.blocks[2].currentPos.col > position:
+        e.append(Event(pygame.KEYDOWN, pygame.K_LEFT))
+        e.append(Event(pygame.KEYUP, pygame.K_LEFT))
     else:
-        e = Event(pygame.KEYDOWN, pygame.K_DOWN)
-    return [e]
+        e.append(Event(pygame.KEYDOWN, pygame.K_DOWN))
+        e.append(Event(pygame.KEYUP, pygame.K_DOWN))
+    # for i in range(0, 4):
+    #     print('col num', game_field.piece.blockMat[i])
+    #     print('status', game_field.piece.status)
+    return e
 
 
-def simulate(game_field, game_figure_image, game_width=10, game_height=20):
-    while not game_figure_image.movCollisionCheck_BLOCK("down", game_figure_image.rowNum):
-        game_figure_image.rowNum += 1
-    game_figure_image.rowNum -= 1
+def simulate(game, game_figure_image, game_width=10, game_height=20):
+    game_field = copy.deepcopy(game.blockMat)
+    # print(game_field)
+    new_game_figure_image = copy.deepcopy(game_figure_image)
+    # while not new_game_figure_image.movCollisionCheck("down"):
+    #     new_game_figure_image.createNextMove("down")
+
+    # while not new_game_figure_image.movCollisionCheck("down"):
+    #     new_game_figure_image.rowNum -= 1
+    #     print(new_game_figure_image.rowNum)
+
+    while not new_game_figure_image.movCollisionCheck("down"):
+        new_game_figure_image.createNextMove("down")
+        new_game_figure_image.applyNextMove()
+    for i in range(0, 4):
+        game_field[new_game_figure_image.blocks[i].currentPos.row][new_game_figure_image.blocks[i].currentPos.col] = \
+            new_game_figure_image.type
     height = game_height
     holes = 0
     filled = []
     for i in range(game_height-1, -1, -1):
         for j in range(game_width):
             u = '_'
-            if game_field[i][j] != 0:
+            if game_field[i][j] != "empty":
                 u = "x"
             for ii in range(4):
                 for jj in range(4):
-                    if ii * 4 + jj in game_figure_image:
-                        if jj + game_figure_image.colNum == j and ii + game_figure_image.rowNum == i:
+                    if ii * 4 + jj in new_game_figure_image.blockMat:
+                        if jj + new_game_figure_image.colNum == j and ii + new_game_figure_image.rowNum == i:
                             u = "x"
 
             if u == "x" and i < height:
@@ -71,31 +88,63 @@ def simulate(game_field, game_figure_image, game_width=10, game_height=20):
                     if (k, j) not in filled:
                         holes += 1
                         filled.append((k, j))
-
     return holes, game_height-height
 
 
-def best_rotation_position(game_field, game_figure, game_width, game_height):
+def best_rotation_position(game_field, game_width, game_height):
     best_height = game_height
     best_holes = game_height*game_width
     best_position = None
     best_rotation = None
     rotation = 0
     for i in range(4):
-        new_game_figure = copy.deepcopy(game_figure)
-        new_game_figure.rotate('cCW')
+        new_game_figure = copy.deepcopy(game_field.piece)
+        new_game_figure.rotate('CW')
         rotation += 1
+        # move to left
+        atLeft = False
+        for j in range(game_width//2):
+            new_game_figure1 = copy.deepcopy(new_game_figure)
+            new_game_figure1.createNextMove("left")
+            new_game_figure1.applyNextMove()
+            for i in range(4):
+                if new_game_figure1.blocks[i].currentPos.row <= -1 or new_game_figure1.blocks[i].currentPos.col <= -1:
+                    atLeft = True
+                    break
+            if atLeft:
+                break
+            new_game_figure = new_game_figure1
+        # for j in range(game_width):
+        #     new_game_figure.createNextMove("left")
+        #     new_game_figure.applyNextMove()
         for j in range(game_width):
-            print(new_game_figure.colNum)
-            if not new_game_figure.movCollisionCheck_BLOCK("down", new_game_figure.colNum):
+            atRight = False
+            if not new_game_figure.movCollisionCheck("down"):
                 holes, height = simulate(
                     game_field,
-                    new_game_figure
+                    new_game_figure,
+                    game_width,
+                    game_height
                 )
+                print(holes, height)
                 if best_position is None or best_holes > holes or \
                         best_holes == holes and best_height > height:
                     best_height = height
                     best_holes = holes
                     best_position = j
                     best_rotation = rotation
+            # new_game_figure.createNextMove("right")
+            # new_game_figure.applyNextMove()
+
+            new_game_figure1 = copy.deepcopy(new_game_figure)
+            new_game_figure1.createNextMove("right")
+            new_game_figure1.applyNextMove()
+            for i in range(4):
+                if new_game_figure1.blocks[i].currentPos.row > game_width - 1 or new_game_figure1.blocks[i].currentPos.col > game_width - 1:
+                    atRight = True
+                    break
+            if atRight:
+                break
+            new_game_figure = new_game_figure1
+
     return best_rotation, best_position
