42e5633aa56e631d40f1d98df80d3f9b1f643096
brandon-hoang-le <55907638+brandon-hoang-le@users.noreply.github.com>
2021-12-05 21:56:47 -0800

    Update tetrisAI.py

--- tetrisAI.py
+++ tetrisAI.py
@@ -1,24 +1,22 @@
-from math import e
 import copy
 import time
 
-def geneticAlgorithm(height, complete_lines, holes, bumpiness, max_height):
+def combine_heuristics(height, complete_lines, holes, bumpiness, max_height):
     return height * -0.510066 + complete_lines * \
-        0.760666 + holes * -0.35663 + bumpiness * -0.184483 + max_height * -0.0000001
+        0.760666 + holes * -0.35663 + bumpiness * -0.184483 + max_height * -0.001
 
 def run_ai(game_field, game_width, game_height):
     game_field.piece.createNextMove('down')
     game_field.piece.applyNextMove()
-    e = []
     if game_field.piece.status != 'moving':
         return []
-    rotation_count = 0
-    best_pos, best_rotation = simulate(game_field, game_width, game_height)
 
+    # simulate all possible outcomes
+    best_pos, best_rotation = simulate(game_field, game_width, game_height)
+    # apply drop
     for i in range(best_rotation):
         time.sleep(0.2)
         game_field.piece.rotate("CW")
-
     shift = game_field.piece.blocks[0].currentPos.col - best_pos[0]
     if shift > 0:
         for i in range(shift):
@@ -31,8 +29,8 @@ def run_ai(game_field, game_width, game_height):
     while not game_field.piece.movCollisionCheck("down"):
         game_field.piece.createNextMove('down')
         game_field.piece.applyNextMove()
-
     return[]
+
 
 def simulate(game, game_width, game_height):
     game1 = copy.deepcopy(game)
@@ -40,17 +38,19 @@ def simulate(game, game_width, game_height):
     best_position = None
     best_rotation = None
     best_rating = None
-    # best_blockMat = None
+    best_blockMat = None
     best_data = []
     cur_rotation = 0
+    # simulate all 4 rotations
     for x in range(4):
-        # rotate_count = 0
         gameX = copy.deepcopy(game1)
         gameXp = gameX.piece
         while not gameXp.movCollisionCheck("left"):
             gameXp.createNextMove('left')
             gameXp.applyNextMove()
+        # simulate all columns
         for ii in range(game_width):
+            # place simulated blocks
             game2 = copy.deepcopy(gameX)
             piece = game2.piece
             blockMat = game2.blockMat
@@ -58,27 +58,30 @@ def simulate(game, game_width, game_height):
                 piece.createNextMove('down')
                 piece.applyNextMove()
             for i in range(4):
-                # If I change 'PLACEHOLDER' to 'empty', the AI does not work correctly. This may be due to a conditional statement that == 'empty'
-                blockMat[piece.blocks[i].currentPos.row][piece.blocks[i].currentPos.col] = 'PLACEHOLDER'
-            a_height, cleared, holes, bumpiness, highest = calc(blockMat, game_width, game_height)
-            rating = geneticAlgorithm(a_height, cleared, holes, bumpiness, highest)
-
-            #  This should clear all the lines
+                blockMat[piece.blocks[i].currentPos.row
+                         ][piece.blocks[i].currentPos.col] = 'SIM'
+            # if there are rows beling cleared, clear and drop free blocks
             cl = game2.getCompleteLines()
+            cl.sort(reverse=True)
             line_cleared = 0
             for i in range(4):
                 if cl[i] != -1:
                     line_cleared += 1
-
-            #  This should clears the rows
+                else:
+                    break
             if line_cleared > 0:
-                for row in cl:
-                    if row != -1:
-                        # This clears completed row from blockMat and inserts new empty list to blockMat (pushing everything down)
-                        for i in range(line_cleared):
-                            blockMat.pop(row)
-                            blockMat.insert(0,['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'])
-            
+                for i in range(line_cleared):
+                    blockMat.pop(cl[i])
+                for i in range(line_cleared):
+                    blockMat.insert(0, ['empty', 'empty', 'empty', 'empty',
+                                        'empty', 'empty', 'empty', 'empty', 'empty', 'empty'])
+            # calculate agregate height, buumpiness, highest height,and holes
+            a_height, cleared, holes, bumpiness, highest = calc_heuristics(
+                blockMat, game_width, game_height)
+            # calculate the rating of this simulation through genetic algorithm
+            rating = combine_heuristics(
+                a_height, line_cleared, holes, bumpiness, highest)
+            # record the best simulation
             if best_rating is None or rating > best_rating:
                 best_rating = rating
                 cur_pos = [0] * 4
@@ -86,11 +89,9 @@ def simulate(game, game_width, game_height):
                     cur_pos[i] = piece.blocks[i].currentPos.col
                 best_position = cur_pos
                 best_rotation = cur_rotation
-                # best_blockMat = blockMat
+                best_blockMat = blockMat
                 best_data = [a_height, cleared, holes,
                              bumpiness, highest, best_rating]
-                if cleared > 0:
-                    print(blockMat)
 
             if not gameXp.movCollisionCheck("right"):
                 gameXp.createNextMove('right')
@@ -101,7 +102,7 @@ def simulate(game, game_width, game_height):
         cur_rotation += 1
     print("aggregate height:", best_data[0], "line cleared:", best_data[1],
           "holes:", best_data[2], "bumpiness:", best_data[3], 'highest height:', best_data[4], 'value:', best_data[5])
-    # print(best_blockMat)
+    print(best_blockMat)
     return best_position, best_rotation
 
 
@@ -114,10 +115,10 @@ def calc_all_heights(blockMat, game_width, game_height):
     return height
 
 
-def calc(blockMat, game_width, game_height):
+def calc_heuristics(blockMat, game_width, game_height):
     h = calc_all_heights(blockMat, game_width, game_height)
+
     # calculate aggregate height
-    max_height = max(h)
     a_height = 0
     for height in h:
         a_height += height
@@ -129,7 +130,7 @@ def calc(blockMat, game_width, game_height):
     bumpiness = 0
     for i in range(0, len(h) - 1):
         bumpiness += abs(h[i] - h[i+1])
-
+        
     # calculate lines cleared
     cleared = 0
     for i in range(game_height - 1, -1, -1):
@@ -140,8 +141,8 @@ def calc(blockMat, game_width, game_height):
             zeros += 1
         if zeros == game_width:
             cleared += 1
-
-    # calculate height
+            
+    # calculate holes
     holes = 0
     for c in range(game_width):
         block = False
